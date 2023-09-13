95ab320120661bcadcd2b63d85c1d25d5b9e5175
ntijoh-nicolai-zackrisson <nicolai.zackrisson@elev.ga.ntig.se>
2023-09-13 15:12:04 +0200

    test newnewnew

--- lib/pluggy/models/fruit.ex
+++ lib/pluggy/models/fruit.ex
@@ -3,10 +3,39 @@ defmodule Pluggy.Fruit do
 
   alias Pluggy.Fruit
 
+#  def all do
+#     Postgrex.query!(DB, "SELECT * FROM fruits", [], pool: DBConnection.ConnectionPool).rows
+#     |> to_struct_list
+
+#    Postgrex.query!(DB, "
+#      SELECT pizzas_ingredients_rel.pizza_id, pizzas_ingredients_rel.ing_id, pizzas.name, ingredients.name
+#      FROM pizzas
+#      INNER JOIN pizzas_ingredients_rel ON pizzas_ingredients_rel.pizza_id=pizzas.id
+#      INNER JOIN ingredients ON ingredients.id=pizzas_ingredients_rel.ing_id",
+#      [], pool: DBConnection.ConnectionPool).rows
+#   |> grouped_results()
+
+#  end
+
   def all do
-    Postgrex.query!(DB, "SELECT * FROM fruits", [], pool: DBConnection.ConnectionPool).rows
-    |> to_struct_list
+    query = """
+      SELECT pizzas.name, pizzas_ingredients_rel.pizza_id, pizzas_ingredients_rel.ing_id, ingredients.name
+      FROM pizzas
+      INNER JOIN pizzas_ingredients_rel ON pizzas_ingredients_rel.pizza_id = pizzas.id
+      INNER JOIN ingredients ON ingredients.id = pizzas_ingredients_rel.ing_id
+    """
+
+    # Execute the SQL query
+    result = Postgrex.query!(DB, query, [], pool: DBConnection.ConnectionPool).rows
+
+    result
+    |> grouped_results()
   end
+
+  def all_ingredients do
+    Postgrex.query!(DB, "SELECT DISTINCT name FROM ingredients" , [], pool: DBConnection.ConnectionPool).rows
+  end
+
 
   def get(id) do
     Postgrex.query!(DB, "SELECT * FROM fruits WHERE id = $1 LIMIT 1", [String.to_integer(id)],
@@ -50,4 +79,9 @@ defmodule Pluggy.Fruit do
   def to_struct_list(rows) do
     for [id, name, tastiness] <- rows, do: %Fruit{id: id, name: name, tastiness: tastiness}
   end
+
+  def grouped_results(rows) do
+    for [pizza, pizza_id, ing_id, ing] <- rows, do: %{id: pizza_id, ing_id: ing_id, ingredient: ing}
+  end
+
 end
